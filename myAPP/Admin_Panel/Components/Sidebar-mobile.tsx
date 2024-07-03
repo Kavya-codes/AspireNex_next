0164878c2104698ef70a5e7506f57f96728cf5d0
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-03 14:46:15 +0530

    Create Sidebar-mobile.tsx

---
+++ myAPP/Admin_Panel/Components/Sidebar-mobile.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+
+const MobileSidebar = ({ children }: { children: React.ReactNode }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onClose);
+  const isOpen = useMobileSidebar((state) => state.isOpen);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [onClose]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        className="block md:hidden mr-2"
+        variant="default"
+        size="sm"
+      >
+        <Menu className="h-5 w-5" />
+      </Button>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent
+          side="left"
+          className="p-2 pt-10 bg-gray-800 text-white"
+        >
+          {children}
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
+
+export default MobileSidebar;
