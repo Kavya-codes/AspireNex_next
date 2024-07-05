8bed897bb00fcd07e3e4b6b785b33f68a619f7c6
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-05 23:29:32 +0530

    Create file1.tsx

---
+++ myAPP/Admin_Panel/admin/product/new/file1.tsx
@@ -0,0 +1,11 @@
+import ProductTable from "./_components/table-products";
+
+const ProductsPage = () => {
+  return (
+    <div className="p-4 mt-2 w-3/4 max-md:w-full mx-auto">
+      <ProductTable />
+    </div>
+  );
+};
+
+export default ProductsPage;
