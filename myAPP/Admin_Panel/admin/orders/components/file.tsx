a55cca57fcbce83e9389999a0078a7fbdb045526
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-05 23:06:09 +0530

    Create file.tsx

---
+++ myAPP/Admin_Panel/admin/orders/components/file.tsx
@@ -0,0 +1,11 @@
+import OrderTable from "./_components/order-table";
+
+const OrdersListPage = () => {
+  return (
+    <div className="padding-4 margin-top-2 width-three-quarters max-md-width-full center-horizontal">
+      <OrderTable />
+    </div>
+  );
+};
+
+export default OrdersListPage;
