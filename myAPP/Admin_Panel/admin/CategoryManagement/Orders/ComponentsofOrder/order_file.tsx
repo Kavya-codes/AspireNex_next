ea8a2324fec9a8c421c1c03c5a4e461943dfd780
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-05 15:00:33 +0530

    Create order_file.tsx

---
+++ myAPP/Admin_Panel/admin/CategoryManagement/Orders/ComponentsofOrder/order_file.tsx
@@ -0,0 +1,11 @@
+import OrdersTable from "./_components/orders-table";
+
+const OrderManagementPage = () => {
+  return (
+    <div className="p-4 mt-2 w-3/4 max-md:w-full mx-auto">
+      <OrdersTable />
+    </div>
+  );
+};
+
+export default OrderManagementPage;
