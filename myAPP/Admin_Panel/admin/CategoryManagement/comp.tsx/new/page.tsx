4d00fa08576254877640d7a51652bf03d6520d28
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-05 14:50:31 +0530

    Create page.tsx

---
+++ myAPP/Admin_Panel/admin/CategoryManagement/comp.tsx/new/page.tsx
@@ -0,0 +1,11 @@
+import CategoriesTable from "./components/CategoriesTable";
+
+const CategoriesOverviewPage = () => {
+  return (
+    <div className="p-4 mt-2 w-3/4 max-md:w-full mx-auto">
+      <CategoriesTable />
+    </div>
+  );
+};
+
+export default CategoriesOverviewPage;
