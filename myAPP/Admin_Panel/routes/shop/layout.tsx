59feb180088dc2b1f3ed5a11b69148b65bfd23d6
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-06 15:54:56 +0530

    Create layout.tsx

---
+++ myAPP/Admin_Panel/routes/shop/layout.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx (example page)
+import Layout from '@/components/Layout';
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1>Homepage Content</h1>
+      <p>Welcome to our website!</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
