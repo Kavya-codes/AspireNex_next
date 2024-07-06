3f34ca8f002d029943c6f7e22f987aeecca56cf2
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-06 15:14:42 +0530

    Create layout.tsx

---
+++ myAPP/Admin_Panel/routes/cart/layout.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Cart | Kemal Store",
+  description: "Cart for e-commerce, selling products, and new productivity",
+};
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
