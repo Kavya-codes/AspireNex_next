6a8b05dd0de27e474f5487c4ac7631b630bbf487
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-06 15:57:55 +0530

    Create sidebar_products_component.tsx

---
+++ myAPP/Admin_Panel/routes/shop/sidebar_products_component.tsx
@@ -0,0 +1,11 @@
+// components/sidebar/SidebarProducts.tsx
+const SidebarProducts = () => {
+  return (
+    <div className="w-1/6 max-sm:w-full p-4 flex flex-col gap-y-1">
+      <p className="font-semibold mt-1">Sidebar Content</p>
+      {/* Insert your sidebar items component here */}
+    </div>
+  );
+};
+
+export default SidebarProducts;
